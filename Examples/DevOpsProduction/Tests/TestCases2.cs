2bc65715c963e903c1fbbf3cd69881fd41838bfe
poornimayo <poornimayo@gmail.com>
2020-07-11 13:21:20 +0530

    done

--- Examples/DevOpsProduction/Tests/TestCases2.cs
+++ Examples/DevOpsProduction/Tests/TestCases2.cs
@@ -9,7 +9,7 @@ using DevOps.Production;
 // These tests are written to have productiuon like tests, to produce many states of a test. Result of this data is used in the dashboards.
 namespace DevOps.Tests.Production2 {
     [TestFixture]
-    public class Tester : DevOps.Tests.Production1.Tester {
+    public class Tester : DevOps.Tests.Production1.Testr {
         // No need to write any tests
     }
 }
